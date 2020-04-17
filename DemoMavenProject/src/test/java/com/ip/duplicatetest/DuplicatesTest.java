e21831930495b8a9b4a36c683268a82c7cd4a593
AlifMondal <alifmondal241@gmail.com>
2020-04-16 17:06:14 -0700

    first

---
+++ DemoMavenProject/src/test/java/com/ip/duplicatetest/DuplicatesTest.java
@@ -0,0 +1,58 @@
+package com.ip.duplicatetest;
+
+
+import com.ip.duplicate.Duplicates;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DuplicatesTest {
+    @Test
+    public void testNormalDuplicates() {
+
+        Assert.assertEquals(Duplicates.getDuplicates("AABBCC"), "ABC");
+    }
+
+    @Test
+    public void testNoDuplicates() {
+
+        Assert.assertEquals(Duplicates.getDuplicates("ABC"),"");
+    }
+
+    @Test
+    public void testFirstDuplicates() {
+
+        Assert.assertEquals(Duplicates.getDuplicates("AABC"), "A");
+    }
+
+    @Test
+    public void testLastDuplicates() {
+        String act=Duplicates.getDuplicates("ABCC");
+        String exp="C";
+        Assert.assertEquals(act, exp);
+    }
+
+    @Test
+    public void testNullString() {
+
+        Assert.assertNull(Duplicates.getDuplicates(null));
+    }
+
+    @Test
+    public void testEmptyString() {
+        String actual=Duplicates.getDuplicates("");
+
+        Assert.assertNull(actual);
+    }
+
+    @Test
+    public void testSingleCharString() {
+
+        Assert.assertEquals(Duplicates.getDuplicates("A"),"");
+    }
+
+    @Test
+    public void testCaseSensitiveString() {
+
+        Assert.assertEquals(Duplicates.getDuplicates("AabbCc"), "b");
+    }
+}
